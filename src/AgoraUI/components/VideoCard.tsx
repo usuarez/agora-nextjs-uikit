37e38d208a9f5e17a2f7db0aab956499a3a280b0
Ubaldo Suarez <sarcosas.dev@gmail.com>
2022-08-08 17:14:53 -0400

    fix ISessionUser

--- src/AgoraUI/components/VideoCard.tsx
+++ src/AgoraUI/components/VideoCard.tsx
@@ -6,37 +6,22 @@ import {
 import Image from "next/image";
 import { FC } from "react";
 import styles from "styles/Agora.module.scss";
-import { ISessionUser } from "types/Agora";
 import { AgoraVideoPlayer } from "./AgoraVideoPlayer";
 
 interface IAgoraVideoCard {
   videoTrack?: ICameraVideoTrack | null;
   remoteVideoTrack?: IRemoteVideoTrack | null;
   isRemote?: boolean;
-  user: ISessionUser;
 }
 
 export const VideoCard: FC<IAgoraVideoCard> = ({
   videoTrack = null,
   remoteVideoTrack = null,
-  user,
 }) => {
   const AvatarTemplate = () => {
     return (
       <div className={styles.avatar}>
-        {!!user.profilePicture ? (
-          <Image
-            src={user.profilePicture}
-            width={140}
-            height={140}
-            objectFit="cover"
-            alt="user profile picture"
-          />
-        ) : (
-          <p className={styles.userInitial}>
-            {!!user && user?.name?.slice(0, 1)}
-          </p>
-        )}
+        <p className={styles.userInitial}>U</p>
       </div>
     );
   };
@@ -44,10 +29,10 @@ export const VideoCard: FC<IAgoraVideoCard> = ({
   return (
     <div className={`${styles.videoCard}`}>
       {!!videoTrack && !videoTrack.muted && (
-        <AgoraVideoPlayer id={user.id!} track={videoTrack!} />
+        <AgoraVideoPlayer id={Math.random()} track={videoTrack!} />
       )}
       {!!remoteVideoTrack && (
-        <AgoraVideoPlayer id={user.id!} track={remoteVideoTrack!} />
+        <AgoraVideoPlayer id={Math.random()} track={remoteVideoTrack!} />
       )}
 
       {!remoteVideoTrack && !!videoTrack?.muted && AvatarTemplate()}
